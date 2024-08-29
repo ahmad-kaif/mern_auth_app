fda652bbcf3ebb89a154408fb0bb82c97610a1cc
Ahmad Kaif <heyahmad611@gmail.com>
2024-08-29 23:27:27 +0530

    signup functionality added

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,57 @@
+import User from "../models/user.model.js";
+import generaateTokenAndSetCookie from "../utils/generateToken.utils.js";
+
+
+export const signup = async (req,res) => {
+    try {
+        const {fullname,username,password,confirmPassword,gender} = req.body;
+
+        if(password !== confirmPassword){
+            return res.status(400).json({error:"Passwords do not match"})
+        }
+
+        const user = await User.findOne({username});
+        if(user){
+            return res.status(400).json({error:"Passwords do not match"})
+        }
+
+        //hash password here
+        const salt  = await bcrypt.genSalt(10);
+        const hashedPassword =  await bcrypt.hash(password,salt);
+
+
+        const newUser  = await User({
+            fullname,
+            username,
+            password:hashedPassword,
+            gender
+        })
+        
+        if(newUser){
+            //generate jwt token here
+            generaateTokenAndSetCookie(newUser._id,res);
+
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                username: newUser.username
+            })
+        }else{
+            return res.status(400).json({error: "Inavlid USer data"})
+        }
+
+
+    } catch (error) {
+        console.log("error in sign up controller",error.message);
+        res.status(500).json({error:"Internal server error"})
+    }
+}
+
+export const login =  async (req,res) => {
+    // res.send(`login  page`);
+}
+
+export const logout = async (req,res) => {
+    // res.send(`logout  page`);
+}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
+import authRoutes from "./routes/auth.routes.js"
+dotenv.config();
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
-app.listen(3000, () => console.log(`server running on port 3000`));
+// app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 3000;
+
+app.use("/api/auth",authRoutes);
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+
+// app.get('/login', (req, res) => {
+//     res.send('Login Page');
+// });
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
