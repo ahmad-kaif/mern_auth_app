d5d11c01296ece80e01efedf1a5d942adb8d5dd3
Ahmad Kaif <heyahmad611@gmail.com>
2024-09-03 14:32:07 +0530

    made authContext in client side

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react'
+import { useAuthContext } from '../context/AuthContext';
 
 const useSignup = () => {
     const [loading,setLoading] = useState(false);
+    const {authUser,setAuthUSer} = useAuthContext();
   return (
     <>
     <div>
-        
+
     </div>
       
     </>
