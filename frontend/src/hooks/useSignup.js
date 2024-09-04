169a3ec7f0778d72de1f73fbb1b8af6fbba398f2
Ahmad Kaif <heyahmad611@gmail.com>
2024-09-04 13:23:49 +0530

    .

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react'
 import { useAuthContext } from '../context/AuthContext';
+import { sign } from 'jsonwebtoken';
 
 const useSignup = () => {
     const [loading,setLoading] = useState(false);
     const {authUser,setAuthUSer} = useAuthContext();
-  return (
-    <>
-    <div>
+ 
+    const signup = async({fullname,username,password,confirmPassword,gender}) => {
 
-    </div>
-      
-    </>
-  )
+    }
+  return {loading,signup};
 }
 
 export default useSignup
