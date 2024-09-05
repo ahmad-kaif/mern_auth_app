9c6d15ef9fd6581796ca9d03e3edf26ec105fd02
Ahmad Kaif <heyahmad611@gmail.com>
2024-09-05 16:54:39 +0530

    .

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react'
 import { useAuthContext } from '../context/AuthContext';
 import { sign } from 'jsonwebtoken';
+import toast from 'react-hot-toast';
 
 const useSignup = () => {
     const [loading,setLoading] = useState(false);
     const {authUser,setAuthUSer} = useAuthContext();
  
     const signup = async({fullname,username,password,confirmPassword,gender}) => {
+        try {
+            
+        } catch (error) {
+            toast.error(`Error in SignUp`, error.message);
+        }finally{
+            setLoading(false);
+        }
 
     }
   return {loading,signup};
